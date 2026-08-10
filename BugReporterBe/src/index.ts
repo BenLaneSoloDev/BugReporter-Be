11019224653899a8bd8793462c4855126fabfc7f
Ben-Lane <benjaminkieranlane@gmail.com>
2026-08-10 20:12:44 +0100

    feat: Setup Base HTML Method Routes

--- BugReporterBe/src/index.ts
+++ BugReporterBe/src/index.ts
@@ -5,8 +5,20 @@ const port = 3001;
 const app: Express = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Get Successful");
 });
+
+app.post("/", (req: Request, res: Response) => {
+  res.send("Post Successful")
+})
+
+app.patch("/", (req: Request, res: Response) => {
+  res.send("Patch Successful")
+})
+
+app.delete("/", (req: Request, res: Response) => {
+  res.send("Delete Successful")
+})
 
 app.listen(port, () => {
     console.log("App listening on port: " + port);
