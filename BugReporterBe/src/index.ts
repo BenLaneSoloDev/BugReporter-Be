114e8e61e859a97de10a298400b2ad4f51ba674f
Ben-Lane <benjaminkieranlane@gmail.com>
2026-08-10 21:17:13 +0100

    feat: Setup Request Logs with Morgan

--- BugReporterBe/src/index.ts
+++ BugReporterBe/src/index.ts
@@ -1,8 +1,15 @@
 import type { Express, Request, Response } from "express";
 const express = require("express");
+const fs = require("fs");               // Node.js API
+const path = require("path");           // Node.js API
+const morgan = require("morgan");
+
 
 const port = 3001; 
 const app: Express = express();
+
+let accessLogStream = fs.createWriteStream(path.join(__dirname, "..", "access.log"), { flags: "a" });
+app.use(morgan("combined", { stream: accessLogStream }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Get Successful");
