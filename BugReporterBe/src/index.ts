02b2a6b57c969289a11782debe02b3ce2abad697
Ben-Lane <benjaminkieranlane@gmail.com>
2026-08-11 12:49:08 +0100

    feat: Setup Cors

--- BugReporterBe/src/index.ts
+++ BugReporterBe/src/index.ts
@@ -3,10 +3,14 @@ const express = require("express");
 const fs = require("fs");               // Node.js API
 const path = require("path");           // Node.js API
 const morgan = require("morgan");
-
+const cors= require("cors");
 
 const port = 3001; 
 const app: Express = express();
+
+// ! - CORS must be tested before release to ensure it works correctly
+const corsOptions = { origin: ["http://localhost:3001/"] };
+app.use(cors(corsOptions));
 
 let accessLogStream = fs.createWriteStream(path.join(__dirname, "..", "access.log"), { flags: "a" });
 app.use(morgan("combined", { stream: accessLogStream }));
