dbb0bc7d1d30bcb281b153ae795fb87ec5d07da3
Ben-Lane <benjaminkieranlane@gmail.com>
2026-08-13 12:30:51 +0100

    feat: Setup Project GET Method

--- BugReporterBe/src/projects/providers/getProjects.provider.ts
+++ BugReporterBe/src/projects/providers/getProjects.provider.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const Project = require("../projects.schema.ts");
 
 async function getProjectsProvider(req: Request, res: Response)
 {
@@ -7,9 +8,35 @@ async function getProjectsProvider(req: Request, res: Response)
 
   try {
 
-    
+    const total = await Project.countDocuments();
+    const limit: number = typeof(req.query.limit) === "string" ? parseInt(req.query.limit, 10) : 5; 
+    const page: number = typeof(req.query.page) === "string" ? parseInt(req.query.page, 10) : 1;
+    const baseURL = `${req.protocol}://${req.get("host")}${req.originalUrl.split("?")[0]}`;
 
-    return res.status(StatusCodes.OK).json({ item: "The projects found" });
+    const projects = await Project.find().sort({ title: 1 }); // Grabs all projects in alphabetical order
+
+    const returnData = {
+      data: projects,
+      pagination: {
+        pagination: {
+          meta: {
+            projectsPerPage: limit,
+            totalProjects: total,
+            currentPage: page,
+            totalPages: Math.ceil(total/limit),
+          },
+          links: {
+            first: `${baseURL}?limit=${limit}&page=${1}`,
+            last: `${baseURL}?limit=${limit}&page=${Math.ceil(total/limit)}`,
+            current: `${baseURL}?limit=${limit}&page=${page}`,
+            next: page === Math.ceil(total/limit) ? `` : `${baseURL}?limit=${limit}&page=${page + 1}`,
+            previous: page === 1 ? `` :  `${baseURL}?limit=${limit}&page=${page - 1}`
+          }
+        }
+      }
+    }
+
+    return res.status(StatusCodes.OK).json(returnData);
   }
   catch (error) {
     return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
